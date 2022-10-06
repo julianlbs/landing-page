04a9bb35261b0fc122d819ec0a8c696e2043187b
julianlbs <julianlimalbs@gmail.com>
2022-10-06 15:29:55 -0300

    fix: theme undefined on first render from server

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,33 +2,26 @@ import { useTheme } from "next-themes";
 import { BsFillMoonFill } from "react-icons/bs";
 import { BsFillSunFill } from "react-icons/bs";
 import Link from "next/link";
-import { TypeAnimation } from "react-type-animation";
+import { useEffect, useState } from "react";
 
 export default function CustomHeader() {
+	const [mounted, setMounted] = useState(false);
 	const { theme, setTheme } = useTheme();
 
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) {
+		return null;
+	}
+
 	return (
-		<header className="flex justify-between items-center py-4 px-8 shadow">
-			<h1 className="text-2xl pointer-events-none select-none">
-				<TypeAnimation
-					sequence={[
-						"julian.dev",
-						4000,
-						"julian.FrontEnd",
-						1000,
-						"julian.BackEnd",
-						1000,
-						"julian.FullStack",
-						1000,
-					]}
-					speed={1}
-					cursor={true}
-					repeat={Infinity}
-				/>
-			</h1>
+		<header className="flex justify-between items-center py-4 px-8 shadow w-full sm:absolute md:static">
+			<h1 className="text-2xl pointer-events-none select-none">julian.dev</h1>
 			<div className="flex items-center">
 				<span
-					className="hover:cursor-pointer transition-all duration-300 hover:bg-gray-200 dark:hover:bg-gray-900 rounded-lg p-2"
+					className="hover:cursor-pointer transition-all duration-300 hover:bg-gray-200 dark:hover:bg-gray-900 rounded-lg p-2 sm:hidden md:block"
 					onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
 				>
 					{theme === "dark" ? (
