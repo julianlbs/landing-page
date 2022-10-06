67c25aeb6403e48be98e76073df39b8db2e4b7d7
julianlbs <julianlimalbs@gmail.com>
2022-10-06 10:46:10 -0300

    Add type animation

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,31 @@
 import { useTheme } from "next-themes";
-import { Button } from "@mui/material";
 import { BsFillMoonFill } from "react-icons/bs";
 import { BsFillSunFill } from "react-icons/bs";
 import Link from "next/link";
+import { TypeAnimation } from "react-type-animation";
 
 export default function CustomHeader() {
 	const { theme, setTheme } = useTheme();
 
 	return (
 		<header className="flex justify-between items-center py-4 px-8 shadow">
-			<h1>Julian</h1>
+			<h1 className="text-2xl pointer-events-none select-none">
+				<TypeAnimation
+					sequence={[
+						"julian.dev",
+						4000,
+						"julian.FrontEnd",
+						1000,
+						"julian.BackEnd",
+						1000,
+						"julian.FullStack",
+						1000,
+					]}
+					speed={1}
+					cursor={true}
+					repeat={Infinity}
+				/>
+			</h1>
 			<div className="flex items-center">
 				<span
 					className="hover:cursor-pointer transition-all duration-300 hover:bg-gray-200 dark:hover:bg-gray-900 rounded-lg p-2"
@@ -33,7 +49,7 @@ export default function CustomHeader() {
 						</Link>
 					</li>
 				</ul>
-				<div>algolia search</div>
+				<div>[algolia-search]</div>
 			</div>
 		</header>
 	);
