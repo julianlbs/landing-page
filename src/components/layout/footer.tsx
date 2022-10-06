e68138052494b054741f8d414e74d6004574cf70
julianlbs <julianlimalbs@gmail.com>
2022-10-06 15:25:05 -0300

    Add content to footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,35 @@
+import { IconContext } from "react-icons";
+import { AiFillGithub } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
+import { GiBrazilFlag } from "react-icons/gi";
+import { Container } from "../utils";
+
 export default function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<IconContext.Provider value={{ size: "44px" }}>
+				<Container>
+					<div className="flex flex-row justify-between items-center px-4">
+						<div className="flex flex-col items-center">
+							<p>Brazilian Web Developer</p>
+							<GiBrazilFlag color="limegreen" />
+						</div>
+						<a
+							href="https://github.com/julianlbs"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<AiFillGithub />
+						</a>
+						<a
+							href="mailto:julianlimalbs@gmail.com"
+							className="flex flex-col items-center"
+						>
+							<MdEmail />
+						</a>
+					</div>
+				</Container>
+			</IconContext.Provider>
+		</footer>
+	);
 }
