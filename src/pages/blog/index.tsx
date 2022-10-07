ec6834372539badf3d5223fa521f7d5f87e4556a
julianlbs <julianlimalbs@gmail.com>
2022-10-06 21:45:04 -0300

    Consume strapi api with trpc

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,10 +1,43 @@
-import CustomHeader from "../../components/layout/header";
+import Head from "next/head";
+import { Response } from "node-fetch";
+import { TypeAnimation } from "react-type-animation";
+import Layout from "../../components/layout/layout";
+import { Container } from "../../components/utils";
+import { trpc } from "../../utils/trpc";
 
 export default function BlogHomePage() {
+	const restaurants = trpc.restaurantList.useQuery();
+
 	return (
 		<>
-			<CustomHeader />
-			<div>HELLO WORLD</div>
+			<Head>
+				<title>Julian - Blog home page</title>
+			</Head>
+			<Layout>
+				<main>
+					<Container>
+						{restaurants.isLoading && (
+							<TypeAnimation
+								sequence={[
+									"Loading.",
+									1000,
+									"Loading..",
+									"Loading...",
+									1000,
+									"Loading.....",
+								]}
+							/>
+						)}
+						{!restaurants.isLoading &&
+							restaurants.data?.map((r, i) => (
+								<div key={i}>
+									<h3>{r.attributes.Name}</h3>
+									<p>{r.attributes.Description}</p>
+								</div>
+							))}
+					</Container>
+				</main>
+			</Layout>
 		</>
 	);
 }
