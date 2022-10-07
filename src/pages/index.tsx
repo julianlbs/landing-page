ae3ec8e6020fd3abcc6b49427a5d488ea4a44bf3
julianlbs <julianlimalbs@gmail.com>
2022-10-06 21:42:45 -0300

    refact: separate content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import type { NextPage } from "next";
 import Layout from "../components/layout/layout";
-import { Container } from "../components/utils";
-// import Head from "next/head";
+import HomePage from "./home/HomePage";
 
 const Home: NextPage = () => {
 	return (
 		<Layout>
-			<main>
-				<Container>
-					<div>Hello World!</div>
-				</Container>
-			</main>
+			<HomePage />
 		</Layout>
 	);
 };
