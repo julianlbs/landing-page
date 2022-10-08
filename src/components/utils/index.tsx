131f22949aaa0e2aa73862bf6a80e84614c92f4f
julianlbs <julianlimalbs@gmail.com>
2022-10-08 13:20:12 -0300

    refact: change container padding

--- src/components/utils/index.tsx
+++ src/components/utils/index.tsx
@@ -3,5 +3,5 @@ import React from "react";
 export const Container: React.FC<{ children: React.ReactNode }> = ({
 	children,
 }) => {
-	return <div className="p-4">{children}</div>;
+	return <div className="py-4 px-8">{children}</div>;
 };
