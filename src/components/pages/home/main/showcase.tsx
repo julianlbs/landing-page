bce4cbba586d3438030e00e72abb1ed1706e745c
julianlbs <julianlimalbs@gmail.com>
2022-10-08 15:57:51 -0300

    Add background color to light theme

--- src/components/pages/home/main/showcase.tsx
+++ src/components/pages/home/main/showcase.tsx
@@ -5,7 +5,7 @@ const Showcase = () => {
 	return (
 		<section
 			id="showcase"
-			className="md:w-full dark:bg-gradient-to-r from-gray-700 to-gray-800 grid grid-rows-2 gap-8"
+			className="md:w-full bg-gradient-to-r from-white to-slate-200 dark:from-gray-700 dark:to-gray-800 grid grid-rows-2 gap-8"
 		>
 			<Intro />
 			<Skills />
