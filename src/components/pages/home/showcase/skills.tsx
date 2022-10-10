d98884fa91d73e6ee25d897b3b3f8ce7c4901765
julianlbs <julianlimalbs@gmail.com>
2022-10-10 15:41:56 -0300

    refact: code implovement and Container refactoring

--- src/components/pages/home/showcase/skills.tsx
+++ src/components/pages/home/showcase/skills.tsx
@@ -7,51 +7,49 @@ import { Container } from "../../../utils";
 
 const Skills = () => {
 	return (
-		<div className="dark:bg-slate-800 bg-slate-200">
-			<Container className="px-12 py-4">
+		<IconContext.Provider value={{ className: "text-3xl mb-2" }}>
+			<Container className="dark:bg-slate-800 bg-slate-200">
 				<h2 className="text-center text-primary uppercase mb-12">My Skills</h2>
-				<div className="flex flex-wrap flex-row justify-between items-center select-none">
-					<IconContext.Provider value={{ className: "text-3xl mb-2" }}>
-						<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
-							<span className="hover:animate-spin">
-								<FaReact style={{ color: "aqua" }} />
-							</span>{" "}
-							React
-						</div>
-						<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
-							<span className="hover:animate-spin">
-								<SiTypescript style={{ color: "DodgerBlue" }} />
-							</span>{" "}
-							TypeScript
-						</div>
-						<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
-							<span className="hover:animate-spin">
-								<FaNodeJs style={{ color: "green" }} />
-							</span>{" "}
-							NodeJS
-						</div>
-						<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
-							<span className="hover:animate-spin">
-								<SiNextdotjs style={{ color: "black" }} />
-							</span>{" "}
-							NextJS
-						</div>
-						<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
-							<span className="hover:animate-spin">
-								<TbBrandReactNative style={{ color: "DarkMagenta" }} />
-							</span>{" "}
-							React Native
-						</div>
-						<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
-							<span className="hover:animate-spin">
-								<SiStrapi style={{ color: "darkslateblue" }} />
-							</span>{" "}
-							Strapi
-						</div>
-					</IconContext.Provider>
+				<div className="flex flex-wrap flex-row justify-between items-center select-none ">
+					<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
+						<span className="hover:animate-spin">
+							<FaReact style={{ color: "aqua" }} />
+						</span>{" "}
+						React
+					</div>
+					<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
+						<span className="hover:animate-spin">
+							<SiTypescript style={{ color: "DodgerBlue" }} />
+						</span>{" "}
+						TypeScript
+					</div>
+					<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
+						<span className="hover:animate-spin">
+							<FaNodeJs style={{ color: "green" }} />
+						</span>{" "}
+						NodeJS
+					</div>
+					<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
+						<span className="hover:animate-spin">
+							<SiNextdotjs style={{ color: "black" }} />
+						</span>{" "}
+						NextJS
+					</div>
+					<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
+						<span className="hover:animate-spin">
+							<TbBrandReactNative style={{ color: "DarkMagenta" }} />
+						</span>{" "}
+						React Native
+					</div>
+					<div className="flex flex-col items-center hover:scale-150 hover:transition-all">
+						<span className="hover:animate-spin">
+							<SiStrapi style={{ color: "darkslateblue" }} />
+						</span>{" "}
+						Strapi
+					</div>
 				</div>
 			</Container>
-		</div>
+		</IconContext.Provider>
 	);
 };
 
