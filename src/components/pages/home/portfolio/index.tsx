e7f75db55c382da100dc2d7285c60ea2b9f7240c
julianlbs <julianlimalbs@gmail.com>
2022-10-10 19:01:51 -0300

    feat: Create portfolio base structure

--- src/components/pages/home/portfolio/index.tsx
+++ src/components/pages/home/portfolio/index.tsx
@@ -1,11 +1,49 @@
 import { Container } from "../../../utils";
 
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { IconContext } from "react-icons";
+import { PORTFOLIO } from "../../../../constants/portfolio";
+
 export default function Portfolio() {
 	return (
-		<section id="portfolio">
-			<Container>
-				<h2 className="text-center text-primary uppercase mb-12 ">Portfolio</h2>
-			</Container>
-		</section>
+		<IconContext.Provider value={{ className: "text-2xl text-accent" }}>
+			<section id="portfolio">
+				<Container>
+					<h2 className="text-center text-primary uppercase mb-12 ">
+						Portfolio
+					</h2>
+					<div className="flex flex-col w-full md:flex-row">
+						<div className="flex-[1] card bg-base-300 rounded-box md:mr-2">
+							{PORTFOLIO.map((p) => (
+								<>
+									<Container
+										className="py-6 flex justify-between items-center hover:bg-base-200 overflow-hidden select-none cursor-pointer"
+										onClick={() => console.log("click")}
+									>
+										<div>
+											<h3 className="text-accent">{p.name}</h3>
+											<p className="text-sm text-secondary">
+												{p.description.about}
+											</p>
+										</div>
+										<MdKeyboardArrowRight />
+									</Container>
+									<div
+										className="bg-secondary opacity-20 mx-auto"
+										style={{ width: "90%", height: "1px" }}
+									></div>
+								</>
+							))}
+							<div className="btn-group flex justify-center my-4">
+								<button className="btn btn-active">1</button>
+							</div>
+						</div>
+						<div className="grid flex-[2] h-32 card bg-base-300 rounded-box place-items-center">
+							content
+						</div>
+					</div>
+				</Container>
+			</section>
+		</IconContext.Provider>
 	);
 }
---
+++ src/constants/portfolio.ts
@@ -0,0 +1,6 @@
+export type Portfolio = typeof PORTFOLIO;
+
+export const PORTFOLIO = [
+  { name: "Soil App", description: { about: "React Native App", platform: "Android/iOS", language: "TypeScript", Frameworks: "React Native" }, },
+  { name: "LTClean", description: { about: "React Landing Page", platform: "Web", language: "JavaScript", Frameworks: "BootStrap, React" } },
+];
