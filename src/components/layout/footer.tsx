655d10de9069d641d4faa53756e1336722405b37
julianlbs <julianlimalbs@gmail.com>
2022-10-10 23:13:49 -0300

    temporary footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,33 +3,14 @@ import { AiFillGithub } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
 import { GiBrazilFlag } from "react-icons/gi";
 import { Container } from "../utils";
+import { format } from "date-fns";
 
 export default function Footer() {
 	return (
-		<footer>
-			<IconContext.Provider value={{ size: "44px" }}>
-				<Container>
-					<div className="flex flex-row justify-between items-center px-4">
-						<div className="flex flex-col items-center">
-							<p>Brazilian Web Developer</p>
-							<GiBrazilFlag color="limegreen" />
-						</div>
-						<a
-							href="https://github.com/julianlbs"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<AiFillGithub />
-						</a>
-						<a
-							href="mailto:julianlimalbs@gmail.com"
-							className="flex flex-col items-center"
-						>
-							<MdEmail />
-						</a>
-					</div>
-				</Container>
-			</IconContext.Provider>
+		<footer className="footer footer-center p-4 bg-base-300 text-base-content">
+			<div>
+				<p>Copyright © {format(new Date(), "yyyy")} - All rights reserved</p>
+			</div>
 		</footer>
 	);
 }
