6e6ffe4bdadd7162f552ccea67d1439eb87f2bea
julianlbs <julianlimalbs@gmail.com>
2022-10-12 18:47:50 -0300

    feat: Create Individual post page

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout/layout";
+
+export default function Post() {
+	return (
+		<Layout>
+			<div>Hello, this is my post</div>
+		</Layout>
+	);
+}
+
+const getStaticProps = async () => {};
