321f954fe62ae6bcbbeee69ddef0559cceb78d92
julianlbs <julianlimalbs@gmail.com>
2022-10-13 12:55:35 -0300

    feat: Create route to get one article

--- src/server/routers/app.ts
+++ src/server/routers/app.ts
@@ -1,13 +1,21 @@
 import { initTRPC } from '@trpc/server';
 import { z } from 'zod';
-import { Articles } from '../../core/blogTypes';
+import { Article, Articles } from '../../core/blogTypes';
 import { fetchBlogAPI } from '../../lib/blog/api';
 
 export const t = initTRPC.create();
+
 export const appRouter = t.router({
-  articles: t.procedure
+  getArticles: t.procedure
     .query(async () => {
       const response: Articles = await fetchBlogAPI("/articles", { populate: ["image", "category", "author"] });
+      return response.data;
+    }),
+
+  getOneArticle: t.procedure
+    .input(z.object({ slug: z.string() }))
+    .query(async ({ input }) => {
+      const response: { data: Article, meta: {}; } = await fetchBlogAPI(`/articles`, { filters: { slug: { $eq: input.slug } }, populate: ["image", "category", "author"] }, { encodeValuesOnly: true });
       return response.data;
     }),
 });
