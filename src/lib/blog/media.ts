0165db8b4e2f5b4a31f4dafb1b0cc3c27a441f9d
julianlbs <julianlimalbs@gmail.com>
2022-10-13 16:45:52 -0300

    refact: change function name

--- src/lib/blog/media.ts
+++ src/lib/blog/media.ts
@@ -1,6 +1,6 @@
 import { getStrapiURL } from "./api";
 
-export function getStrapiMedia(media: any) {
+export function getBlogMedia(media: any) {
   const { url } = media.data.attributes;
   const imageUrl: string = url.startsWith("/") ? getStrapiURL(url) : url;
   return imageUrl;
