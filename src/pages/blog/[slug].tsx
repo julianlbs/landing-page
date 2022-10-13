ff7635a50b59b9bcdbd6dda436fdd540c8b88565
julianlbs <julianlimalbs@gmail.com>
2022-10-13 18:35:28 -0300

    refact: show article image

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -11,6 +11,7 @@ import rehypeRaw from "rehype-raw";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import Link from "next/link";
 import Image from "next/image";
+import { getBlogMedia } from "../../lib/blog/media";
 
 interface Props extends React.ComponentPropsWithRef<"div"> {
 	article: Article;
@@ -34,6 +35,17 @@ export default function Post({ article }: Props) {
 			</div>
 			<article>
 				<Container className="max-w-4xl mx-auto">
+					<div className="h-60 w-full overflow-hidden">
+						{article.attributes.image && (
+							<Image
+								src={getBlogMedia(article.attributes.image)}
+								alt={article.attributes.image.data.attributes.caption}
+								height={article.attributes.image.data.attributes.height}
+								width={article.attributes.image.data.attributes.width}
+								objectFit="contain"
+							/>
+						)}
+					</div>
 					<h2 className="text-primary">{article.attributes.title}</h2>
 					<span className="block mb-4">
 						{format(new Date(article.attributes.publishedAt), "PPP")}
